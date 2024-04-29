fd713751856b7aa70c2c6808fb2b90a700e99b09
wolfe-xe <ashwinsudheer5899p@gmail.com>
2024-04-29 13:09:31 +0530

    Rendered a box gameobject

--- GameLoop/Main.cpp
+++ GameLoop/Main.cpp
@@ -3,13 +3,17 @@
 
 #include "Constants.h"
 
-/// <summary>
-/// argc (argument count) - Non - negative value representing the number of arguments passed to the program from the environment in which the program is run.
-/// argv (argument vector) -	Pointer to the first element of an array of argc + 1 pointers, of which the last one is null and the previous ones, if any, point to strings that represent the arguments passed to the program from the host environment. If argv[0] is not a null pointer (or, equivalently, if argc > 0), it points to a string that represents the program name, which is empty if the program name is not available from the host environment.
-/// </summary>
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 int game_is_running = FALSE;
+
+struct Box
+{
+	float x;
+	float y;
+	float width;
+	float height;
+} box;
 
 int Initialize_Window(void) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -55,15 +59,34 @@ void Process_Input() {
 	}
 }
 void Start() {
-	//TODO: 
+	box.x = 20;
+	box.y = 20;
+	box.width = 10;
+	box.height = 10;
 }
 
 void Update() {
-	//TODO:
+	box.x += 1;
+	box.y += 1;
 }
 
 void Render() {
-	//TODO:
+	SDL_SetRenderDrawColor(renderer, 2, 1, 0, 255);
+	SDL_RenderClear(renderer);
+
+	//here is where we can start drawing gameObjects
+	SDL_Rect box_rect = {
+		(int)box.x,
+		(int)box.y,
+		(int)box.width,
+		(int)box.height
+	};
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderFillRect(renderer, &box_rect);
+
+	SDL_RenderPresent(renderer);
+	
 }
 
 void Destroy_Window() {
@@ -72,6 +95,10 @@ void Destroy_Window() {
 	SDL_Quit;
 }
 
+/// <summary>
+/// argc (argument count) - Non - negative value representing the number of arguments passed to the program from the environment in which the program is run.
+/// argv (argument vector) -	Pointer to the first element of an array of argc + 1 pointers, of which the last one is null and the previous ones, if any, point to strings that represent the arguments passed to the program from the host environment. If argv[0] is not a null pointer (or, equivalently, if argc > 0), it points to a string that represents the program name, which is empty if the program name is not available from the host environment.
+/// </summary>
 int main(int argc, char* argv[]) {
 	game_is_running = Initialize_Window();
 
