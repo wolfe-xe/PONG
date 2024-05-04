d57672100150e34f1eb080f62d06c0394a7e8bcf
wolfe-xe <ashwinsudheer5899p@gmail.com>
2024-05-04 12:57:29 +0530

    > made some aesthetical changes to the UI(tried to make it look better)      > this includes the color scheme      > the middle dotted lines      > and the HP ba

--- GameLoop/Constants.h
+++ GameLoop/Constants.h
@@ -3,7 +3,7 @@
 #define FALSE 0
 #define TRUE 1
 
-#define WINDOW_WIDHT 800
+#define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
 #define SPEED 200
--- GameLoop/Main.cpp
+++ GameLoop/Main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <SDL.h>
+#include <random>
 
 #include "Constants.h"
 
@@ -9,16 +10,14 @@ SDL_Renderer* renderer = NULL;
 //int game_is_running = FALSE;
 int last_frame_time = 0;
 
-struct controlls 
-{
+struct controls {
 	int game_is_running = FALSE;
 	int game_is_restarted = FALSE;
 	int up = FALSE;
 	int down = FALSE;
-} controlls;
+} controls;
 
-struct Box 
-{
+struct Box {
 	float x;
 	float y;
 	float width;
@@ -27,8 +26,7 @@ struct Box
 	float y_velocity;
 } box;
 
-struct player
-{
+struct player{
 	float x;
 	float y;
 	float width;
@@ -36,14 +34,48 @@ struct player
 	float y_vel;
 } player;
 
-struct opponent
-{
+struct opponent{
 	float x;
 	float y;
 	float width;
 	float height;
 	float y_vel;
 } opponent;
+
+struct guiSeparator {
+	float x;
+	float y;
+	float width;
+	float height;
+} guiSeparator;
+
+struct p_score {
+	float x;
+	float y;
+	float w;
+	float h;
+}p_score;
+
+struct p_score_max {
+	float x;
+	float y;
+	float w;
+	float h;
+} p_score_max;
+
+struct o_score {
+	float x;
+	float y;
+	float w;
+	float h;
+}o_score;
+
+struct o_score_max {
+	float x;
+	float y;
+	float w;
+	float h;
+} o_score_max;
 
 
 int Initialize_Window(void) {
@@ -55,7 +87,7 @@ int Initialize_Window(void) {
 		"GameLoop", 
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		WINDOW_WIDHT,
+		WINDOW_WIDTH,
 		WINDOW_HEIGHT,
 		SDL_WINDOW_SHOWN
 	);
@@ -76,69 +108,113 @@ void Process_Input() {
 
 	switch (event.type) {
 	case SDL_QUIT:
-		controlls.game_is_running = FALSE;
+		controls.game_is_running = FALSE;
 		break;
 
 	case SDL_KEYDOWN:
 		if (event.key.keysym.sym == SDLK_ESCAPE) {
-			controlls.game_is_running = FALSE;
+			controls.game_is_running = FALSE;
 		}
+		//player controls
 		if (event.key.keysym.sym == SDLK_w) {
-			controlls.up = TRUE;
+			controls.up = TRUE;
 			player.y_vel = -SPEED;
 		}
 		if (event.key.keysym.sym == SDLK_s) {
-			controlls.down = TRUE;
+			controls.down = TRUE;
 			player.y_vel = SPEED;
 		}
+		//opponent controls
 		if (event.key.keysym.sym == SDLK_UP) {
-			controlls.up = TRUE;
+			controls.up = TRUE;
 			opponent.y_vel = -SPEED;
 		}
 		if (event.key.keysym.sym == SDLK_DOWN) {
-			controlls.down = TRUE;
+			controls.down = TRUE;
 			opponent.y_vel = SPEED;
 
 		}
 
 	case SDL_KEYUP:
+		//player
 		if (event.key.keysym.sym == SDLK_w) {
-			controlls.up = FALSE;
+			controls.up = FALSE;
 		}
 		if (event.key.keysym.sym == SDLK_s) {
-			controlls.down = FALSE;
+			controls.down = FALSE;
 		}
+		//opponent
 		if (event.key.keysym.sym == SDLK_UP) {
-			controlls.up = FALSE;
+			controls.up = FALSE;
 		}
 		if (event.key.keysym.sym == SDLK_DOWN) {
-			controlls.down = FALSE;
+			controls.down = FALSE;
 		}
+	}
+}
+
+int RandomDir(int x, int y) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<>	dis(0, 1);
+
+	if (dis(gen) == 0) {
+		return x;
+	}
+	else {
+		return y;
 	}
 }
 
 void Start() {
 	//setting up ball's initial pos
-	box.x = (WINDOW_WIDHT - box.width) / 2;
-	box.y = (WINDOW_HEIGHT - box.height) / 2;
+	box.x = (WINDOW_WIDTH - box.width) / 2;
+	box.y =  (WINDOW_HEIGHT - box.height) / 2;
 	box.width = 10;
 	box.height = 10;
-	box.x_velocity = BALLSPEED;
-	box.y_velocity = BALLSPEED;
+	box.x_velocity = RandomDir(SPEED, -SPEED);
+	box.y_velocity = RandomDir(SPEED, -SPEED);
 	
 	//setting up players initial pos
 	player.x = 15;
 	player.y = (WINDOW_HEIGHT - player.height) / 2;
 	player.width = 15;
 	player.height = 50;
-	player.y_vel = -SPEED;
+	player.y_vel = 0;
 
 	//setting up opponents inital pos
-	opponent.x = WINDOW_WIDHT - 35;
+	opponent.x = WINDOW_WIDTH - 35;
 	opponent.y = (WINDOW_HEIGHT - opponent.height) / 2;
 	opponent.width = 15;
 	opponent.height = 50;
-	opponent.y_vel = SPEED;
+	opponent.y_vel = 0;
+
+	//setting up guiSeparator pos
+	guiSeparator.x = (WINDOW_WIDTH - guiSeparator.width) / 2;
+	guiSeparator.y = 0;
+	guiSeparator.width = 5;
+	guiSeparator.height = 10;
+
+	p_score.x = 130;
+	p_score.y = 15;
+	p_score.h = 100;
+	p_score.w = 10;
+
+	p_score_max.x = 130;
+	p_score_max.y = 15;
+	p_score_max.h = 100;
+	p_score_max.w = 10;
+
+	o_score.x = WINDOW_WIDTH - 230;
+	o_score.y = 15;
+	o_score.h = 100;
+	o_score.w = 10;
+
+	o_score_max.x = WINDOW_WIDTH - 230;
+	o_score_max.y = 15;
+	o_score_max.h = 100;
+	o_score_max.w = 10;
+
 }
 
 void Update() {
@@ -158,6 +234,7 @@ void Update() {
 	float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
 	//store the ms of the current frame to be used in the next one;
 	last_frame_time = SDL_GetTicks();
+
 
 	//control update for player
 	player.y += player.y_vel * delta_time;
@@ -185,24 +262,33 @@ void Update() {
 		opponent.y_vel = -opponent.y_vel;
 	}
 
-	//ball controlls 
+	//ball controls 
 	box.x += box.x_velocity * delta_time;
 	box.y += box.y_velocity * delta_time;
 
 	//collision detection with bounds
 	if (box.x <= 0) {
-		box.x = 0;
+		box.x = (WINDOW_WIDTH - box.width) / 2;
 		box.x_velocity = -box.x_velocity;
-		printf("game over\n");
+		p_score.h -= 10;
+		if (p_score.h == 0) {
+			printf("gameOver\n");
+		}
+		printf("left side\n");
+	}
+	if (box.x >= WINDOW_WIDTH - box.width) {
+		box.x = (WINDOW_WIDTH - box.width) / 2;
+		box.x_velocity = -box.x_velocity;
+		o_score.h -= 10;
+		if (o_score.h == 0) {
+			printf("gameOver\n");
+			controls.game_is_running = FALSE;
+		}
+		printf("right side\n");
 	}
 	if (box.y <= 0) {
 		box.y = 0;
 		box.y_velocity = -box.y_velocity;
-	}
-	if (box.x >= WINDOW_WIDHT - box.width) {
-		box.x = (WINDOW_HEIGHT - box.width) / 2;
-		box.x_velocity = -box.x_velocity;
-		printf("game over\n");
 	}
 	//bottom bounds
 	if (box.y >= WINDOW_HEIGHT - box.height) {
@@ -238,6 +324,41 @@ void Render() {
 		(int)opponent.height
 	};
 
+	SDL_Rect guiSeparator_rect = {
+		(int)guiSeparator.x,
+		(int)guiSeparator.y,
+		(int)guiSeparator.width,
+		(int)guiSeparator.height,
+	};
+
+	SDL_Rect p_score_rect = {
+		(int)p_score.x,
+		(int)p_score.y,
+		(int)p_score.h,
+		(int)p_score.w
+	};
+
+	SDL_Rect p_score_max_rect = {
+		(int)p_score_max.x,
+		(int)p_score_max.y,
+		(int)p_score_max.h,
+		(int)p_score_max.w,
+	};
+
+	SDL_Rect o_score_rect = {
+		(int)o_score.x,
+		(int)o_score.y,
+		(int)o_score.h,
+		(int)o_score.w
+	};
+
+	SDL_Rect o_score_max_rect = {
+		(int)o_score_max.x,
+		(int)o_score_max.y,
+		(int)o_score_max.h,
+		(int)o_score_max.w,
+	};
+
 	//checks for player collision
 	SDL_bool playerCollision = SDL_HasIntersection(&player_rect, &box_rect);
 	if (playerCollision) {
@@ -253,9 +374,31 @@ void Render() {
 		box.y_velocity = -box.y_velocity;
 	}
 
+	SDL_SetRenderDrawColor(renderer, 115, 110, 110, 255);
+	SDL_RenderFillRect(renderer, &p_score_max_rect);
+	SDL_RenderFillRect(renderer, &o_score_max_rect);
+
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderFillRect(renderer, &box_rect);
+
+	SDL_SetRenderDrawColor(renderer, 115, 110, 110, 255);
+	//SDL_RenderFillRect(renderer, &guiSeparator_rect);
+	for (guiSeparator_rect.y = 0; guiSeparator_rect.y < WINDOW_HEIGHT; guiSeparator_rect.y += 28)
+	{
+		SDL_RenderFillRect(renderer, &guiSeparator_rect);
+	}
+
+	//SDL_SetRenderDrawColor(renderer, 5, 255, 5, 255);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderFillRect(renderer, &p_score_rect);
+	SDL_RenderFillRect(renderer, &o_score_rect);
+
+	//SDL_SetRenderDrawColor(renderer, 65, 94, 54, 255);
+	SDL_SetRenderDrawColor(renderer, 146, 153, 144, 255);
 	SDL_RenderFillRect(renderer, &player_rect);
+
+	//SDL_SetRenderDrawColor(renderer, 97, 54, 54, 255);
+	SDL_SetRenderDrawColor(renderer, 153, 144, 144, 255);
 	SDL_RenderFillRect(renderer, &opponent_rect);
 
 	SDL_RenderPresent(renderer);
@@ -273,11 +416,11 @@ void Destroy_Window() {
 /// argv (argument vector) - Pointer to the first element of an array of argc + 1 pointers, of which the last one is null and the previous ones, if any, point to strings that represent the arguments passed to the program from the host environment. If argv[0] is not a null pointer (or, equivalently, if argc > 0), it points to a string that represents the program name, which is empty if the program name is not available from the host environment.
 /// </summary>
 int main(int argc, char* argv[]) {
-	controlls.game_is_running = Initialize_Window();
+	controls.game_is_running = Initialize_Window();
 
 	Start();
 
-	while (controlls.game_is_running) {
+	while (controls.game_is_running) {
 		Process_Input();
 		Update();
 		Render();
@@ -289,8 +432,14 @@ int main(int argc, char* argv[]) {
 }
 
 /// TODO:
-/// restart
-/// better game mechanics
-/// collision detection
-/// random value at start
-/// Graphical elements
+/// []better game mechanics
+/// []better collision detection
+/// [x]random value at start
+///		[]work on randomness
+/// []Graphical elements
+///		[-]middle line separating both halfs
+///		[]score ui
+///	[]possibly a start menu
+///		[]restart
+///		[]pause and play
+
